0cc9381872d695ab92f0ccea7452dada6ada6fd4
Ryo Kajiwara <sylph01@s01.ninja>
2023-12-03 13:50:51 +0900

    [hpke] allocate/initialize context

--- ext/openssl/ossl_hpke_ctx.c
+++ ext/openssl/ossl_hpke_ctx.c
@@ -9,6 +9,7 @@ ossl_hpke_ctx_free(void *ptr)
   OSSL_HPKE_CTX_free(ptr);
 }
 
+/* public */
 const rb_data_type_t ossl_hpke_ctx_type = {
   "OpenSSL/HPKE_CTX",
   {
@@ -17,9 +18,44 @@ const rb_data_type_t ossl_hpke_ctx_type = {
   0, 0, RUBY_TYPED_FREE_IMMEDIATELY
 };
 
+static VALUE
+hpke_ctx_new0(VALUE arg)
+{
+  OSSL_HPKE_CTX *ctx = (OSSL_HPKE_CTX *)arg;
+  VALUE obj;
+
+  obj = rb_obj_alloc(cContext);
+  RTYPEDDATA_DATA(obj) = ctx;
+  return obj;
+}
+
+VALUE
+ossl_hpke_ctx_new(OSSL_HPKE_CTX *ctx)
+{
+  VALUE obj;
+  int status;
+
+  obj = rb_protect(hpke_ctx_new0, (VALUE)ctx, &status);
+  if (status) {
+    OSSL_HPKE_CTX_free(ctx);
+    rb_jump_tag(status);
+  }
+
+  return obj;
+}
+
+/* private */
+static VALUE
+ossl_hpke_ctx_alloc(VALUE klass)
+{
+  return TypedData_Wrap_Struct(klass, &ossl_hpke_ctx_type, NULL);
+}
+
 void
 Init_ossl_hpke_ctx(void)
 {
   mHPKE = rb_define_module_under(mOSSL, "HPKE");
   cContext = rb_define_class_under(mHPKE, "Context", rb_cObject);
+
+  rb_define_alloc_func(cContext, ossl_hpke_ctx_alloc);
 }
