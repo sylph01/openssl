ab84f9519d7aa22ec9b25feeff9788ecf427f07d
Ryo Kajiwara <sylph01@s01.ninja>
2023-12-08 21:18:08 +0900

    [hpke] macro to remove code when OpenSSL is less than 3.2.0

--- ext/openssl/ossl_hpke_ctx.h
+++ ext/openssl/ossl_hpke_ctx.h
@@ -5,15 +5,15 @@ extern VALUE mHPKE;
 extern VALUE cContext;
 extern const rb_data_type_t ossl_hpke_ctx_type;
 
+#if OSSL_OPENSSL_PREREQ(3, 2, 0)
 #define GetHpkeCtx(obj, ctx) do {\
     TypedData_Get_Struct((obj), OSSL_HPKE_CTX, &ossl_hpke_ctx_type, (ctx)); \
     if (!(ctx)) { \
 	rb_raise(rb_eRuntimeError, "OSSL_HPKE_CTX wasn't initialized!");\
     } \
 } while (0)
+#endif
 
 void Init_ossl_hpke_ctx(void);
-
-VALUE ossl_hpke_ctx_new(OSSL_HPKE_CTX *);
 
 #endif
