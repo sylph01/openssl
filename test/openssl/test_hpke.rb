c60f0cf7c61faf874cc913b35a754ca2b1288e59
Ryo Kajiwara <sylph01@s01.ninja>
2026-06-05 11:27:52 +0900

    Change behavior of tests based on FIPS-compatibility

--- test/openssl/test_hpke.rb
+++ test/openssl/test_hpke.rb
@@ -35,11 +35,13 @@ class OpenSSL::TestHPKE < OpenSSL::TestCase
   end
 
   def test_keygen_returns_pkey
-    pkey = OpenSSL::HPKE.keygen_with_suite(x25519_suite)
+    pkey = OpenSSL::HPKE.keygen_with_suite(fips_compatible_suite)
     assert_kind_of(OpenSSL::PKey::PKey, pkey)
   end
 
   def test_keygen_for_all_kems
+    # X25519 and X448 are not FIPS-approved.
+    omit_on_fips
     OpenSSL::HPKE::Suite::KEMS.each_key do |kem|
       suite = OpenSSL::HPKE::Suite.new_with_names(kem, :hkdf_sha256, :aes_128_gcm)
       assert_kind_of(OpenSSL::PKey::PKey,
@@ -54,28 +56,31 @@ class OpenSSL::TestHPKE < OpenSSL::TestCase
     end
   end
 
-  def test_base_mode_roundtrip_x25519
-    assert_hpke_roundtrip(x25519_suite)
-  end
-
-  def test_base_mode_roundtrip_x448
-    assert_hpke_roundtrip(OpenSSL::HPKE::Suite.new_with_names(
-      :dhkem_x448_hkdf_sha512, :hkdf_sha512, :aes_256_gcm))
-  end
-
   def test_base_mode_roundtrip_p256
     assert_hpke_roundtrip(OpenSSL::HPKE::Suite.new_with_names(
       :dhkem_p256_hkdf_sha256, :hkdf_sha256, :aes_128_gcm))
   end
 
+  def test_base_mode_roundtrip_x25519
+    omit_on_fips # X25519 is not FIPS-approved
+    assert_hpke_roundtrip(OpenSSL::HPKE::Suite.new_with_names(
+      :dhkem_x25519_hkdf_sha256, :hkdf_sha256, :aes_128_gcm))
+  end
+
+  def test_base_mode_roundtrip_x448
+    omit_on_fips # X448 is not FIPS-approved
+    assert_hpke_roundtrip(OpenSSL::HPKE::Suite.new_with_names(
+      :dhkem_x448_hkdf_sha512, :hkdf_sha512, :aes_256_gcm))
+  end
+
   def test_base_mode_roundtrip_chacha20poly1305
+    omit_on_fips # ChaCha20-Poly1305 is not FIPS-approved
     assert_hpke_roundtrip(OpenSSL::HPKE::Suite.new_with_names(
       :dhkem_x25519_hkdf_sha256, :hkdf_sha256, :chacha20poly1305))
   end
 
   def test_seal_open_multiple_messages_in_order
-    suite = x25519_suite
-    sender, receiver = paired_contexts(suite)
+    sender, receiver = paired_contexts(fips_compatible_suite)
     messages = ["first", "second", "third"]
     ciphertexts = messages.map { |m| sender.seal("aad", m) }
     opened = ciphertexts.map { |c| receiver.open("aad", c) }
@@ -83,8 +88,7 @@ class OpenSSL::TestHPKE < OpenSSL::TestCase
   end
 
   def test_open_fails_with_wrong_aad
-    suite = x25519_suite
-    sender, receiver = paired_contexts(suite)
+    sender, receiver = paired_contexts(fips_compatible_suite)
     ct = sender.seal("correct aad", "secret")
     assert_raise(OpenSSL::HPKE::HPKEError) do
       receiver.open("wrong aad", ct)
@@ -92,8 +96,7 @@ class OpenSSL::TestHPKE < OpenSSL::TestCase
   end
 
   def test_open_fails_on_tampered_ciphertext
-    suite = x25519_suite
-    sender, receiver = paired_contexts(suite)
+    sender, receiver = paired_contexts(fips_compatible_suite)
     ct = sender.seal("aad", "secret message")
     tampered = ct.dup
     tampered.setbyte(0, tampered.getbyte(0) ^ 0xff)
@@ -103,8 +106,7 @@ class OpenSSL::TestHPKE < OpenSSL::TestCase
   end
 
   def test_export_secret_agreement
-    suite = x25519_suite
-    sender, receiver = paired_contexts(suite)
+    sender, receiver = paired_contexts(fips_compatible_suite)
     sender_secret = sender.export(32, "context label")
     receiver_secret = receiver.export(32, "context label")
     assert_equal(32, sender_secret.bytesize)
@@ -112,12 +114,13 @@ class OpenSSL::TestHPKE < OpenSSL::TestCase
   end
 
   def test_export_different_labels_differ
-    suite = x25519_suite
-    sender, = paired_contexts(suite)
+    sender, = paired_contexts(fips_compatible_suite)
     assert_not_equal(sender.export(32, "label a"), sender.export(32, "label b"))
   end
 
   def test_export_only_suite
+    # The export-only suite here uses X25519, which is not FIPS-approved.
+    omit_on_fips
     suite = OpenSSL::HPKE::Suite.new_with_names(
       :dhkem_x25519_hkdf_sha256, :hkdf_sha256, :export_only)
     sender, receiver = paired_contexts(suite)
@@ -127,7 +130,7 @@ class OpenSSL::TestHPKE < OpenSSL::TestCase
   end
 
   def test_context_cannot_be_reinitialized
-    suite = x25519_suite
+    suite = fips_compatible_suite
     sender = OpenSSL::HPKE::Context::Sender.new(:base, suite)
     assert_raise(OpenSSL::HPKE::HPKEError) do
       sender.send(:initialize, :base, suite)
@@ -140,7 +143,7 @@ class OpenSSL::TestHPKE < OpenSSL::TestCase
   end
 
   def test_string_arguments_are_required
-    suite = x25519_suite
+    suite = fips_compatible_suite
     pkey = OpenSSL::HPKE.keygen_with_suite(suite)
     sender = OpenSSL::HPKE::Context::Sender.new(:base, suite)
     assert_raise(TypeError) { sender.encap(12345, "info") }
@@ -149,9 +152,11 @@ class OpenSSL::TestHPKE < OpenSSL::TestCase
 
   private
 
-  def x25519_suite
+  # DHKEM(P-256, HKDF-SHA256), HKDF-SHA256 and AES-128-GCM are all
+  # FIPS-approved, so this suite works both with and without the FIPS provider.
+  def fips_compatible_suite
     OpenSSL::HPKE::Suite.new_with_names(
-      :dhkem_x25519_hkdf_sha256, :hkdf_sha256, :aes_128_gcm)
+      :dhkem_p256_hkdf_sha256, :hkdf_sha256, :aes_128_gcm)
   end
 
   # The KEM public key passed to #encap is the recipient's public key in the
